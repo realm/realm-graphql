17b2a9a012626cfe56928884c1808e163091f6c3
Maximilian Alexander <mbalex99@gmail.com>
2017-12-04 17:48:15 -0800

    adding graphql test server

--- tests/GraphQLTestServer.ts
+++ tests/GraphQLTestServer.ts
@@ -1,10 +1,14 @@
 import { TestServer } from 'realm-object-server'
-import {} from 'realm-object-server-graphql'
+import { GraphQLService } from '../../realm-object-server-graphql/src/service'
 
+/**
+ * A subclass of a Test ROS Server that adds the graphql service by default
+ */
 export class GraphQLTestServer extends TestServer {
 
     constructor() {
         super()
+        this.addService(new GraphQLService())
     }
 
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,16 +1,16 @@
 import { expect } from 'chai'
-import { TestServer } from 'realm-object-server'
 import { generateFakeDataRealm } from './generate-fake-data'
+import { GraphQLTestServer } from './GraphQLTestServer'
 import * as Realm from 'realm'
 
 describe('integration test', function () {
 
-    let testServer: TestServer
+    let testServer: GraphQLTestServer
     let user: Realm.Sync.User
     let testRealm: Realm
 
     before(async () => {
-        testServer = new TestServer()
+        testServer = new GraphQLTestServer()
         await testServer.start()
         user = Realm.Sync.User.adminUser(testServer.adminToken, `http://${testServer.address}`)
         testRealm = await generateFakeDataRealm(true, `realm://${testServer.address}/test`, user)
