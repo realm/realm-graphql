a1a108007f64993be3d5aa4095b4bbbfd3c2675b
Maximilian Alexander <mbalex99@gmail.com>
2017-12-04 17:44:41 -0800

    adding test graphql server

---
+++ tests/GraphQLTestServer.ts
@@ -0,0 +1,10 @@
+import { TestServer } from 'realm-object-server'
+import {} from 'realm-object-server-graphql'
+
+export class GraphQLTestServer extends TestServer {
+
+    constructor() {
+        super()
+    }
+
+}
